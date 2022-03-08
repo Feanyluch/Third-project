a853ceef8d7ec9ebeed83bf912fe3afddb9d168b
iamfeanyluch <ifeanyioluchi7@gmail.com>
2022-03-08 05:20:03 +0100

    Frontend Mentor

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./output/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF", 
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }
+    },
+  },
+  plugins: [],
+}
